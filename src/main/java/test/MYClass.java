567327e158c1b5837739a3a07e0d5ef675026cf2
VelocityKatrajPune <yjoshi02@gmail.com>
2023-07-06 14:44:12 +0530

    [Velocity test]

--- src/main/java/test/MYClass.java
+++ src/main/java/test/MYClass.java
@@ -5,6 +5,7 @@ public class MYClass {
 	public static void main(String[] args) {
 		System.out.println("Hey again i made it");
 		System.out.println("Done");
+		System.out.println("test again");
 
 	}
 
